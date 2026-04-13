7b0b89795c0c0eba5d171742848151a62dc17d25
zCr1z <christhomasbinoj@gmail.com>
2026-04-13 20:24:45 +0530

    fix: stable release going to stable release of gpa calc with new features

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    tailwindcss(), 
+    tailwindcss(),
     react(),
     VitePWA({
       registerType: 'autoUpdate',
